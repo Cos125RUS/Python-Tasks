8bf6098f0ed0d382b7de0f238f71560906ab211d
Valerii_Panov <val250640@mail.ru>
2022-12-13 19:56:32 +1000

    Task17

--- Block02/Task017.py
+++ Block02/Task017.py
@@ -2,3 +2,27 @@
 Найти произведение элементов на указанных позициях. 
 Позиции хранятся в файле file.txt в одной строке одно число'''
 
+n = int(input('N = '))
+list = []
+sum = 0
+
+if n > 0:
+    kef = 1
+else:
+    kef = -1
+
+for i in range(-n, n+kef, kef):
+    list.append(i)
+
+print(list)
+
+
+pos = open('C:\\repo\Python\Tasks\Block02\\file.txt', 'r')
+
+for num in pos:
+    #print('x =', list[int(num)])
+    sum += list[int(num)]
+
+pos.close()
+
+print('sum =', sum)
