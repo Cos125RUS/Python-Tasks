c951f4a4114b012ec8c1b8bf5474e5e161840d1c
Valerii_Panov <val250640@mail.ru>
2022-12-17 13:15:58 +1000

    Task24

---
+++ Block02/Task024.py
@@ -0,0 +1,16 @@
+#24.В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов. 
+#Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+list = [1.1, 1.2, 3.1, 5, 10.01]
+
+min = round((list[0] - int(list[0])), 2)
+max = round((list[0] - int(list[0])), 2)
+
+for i in range(1, len(list)):
+    if 0 < (list[i] - int(list[i])) > max:
+        max = round((list[i] - int(list[i])), 2)
+    if 0 < (list[i] - int(list[i])) < min:
+        min = round((list[i] - int(list[i])), 2)
+
+difference = (max - min)
+print(difference) 
